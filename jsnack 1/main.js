c0f877a1de2d56aa311bfec649608105a46246a4
LeonardoContino <leoconti2000@hotmail.it>
2022-12-13 15:44:52 +0100

    somma fatta

--- jsnack 1/main.js
+++ jsnack 1/main.js
@@ -1,18 +1,23 @@
 // console.log("js ok");
 // SNACK 1
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
-//  (qui potete usare un prompt). Il programma stampa la somma di tutti i numeri inseriti.
+//  (qui potete usare un prompt).
+//  Il programma stampa la somma di tutti i numeri inseriti.
 
 /*
 1.aggiungi elementi nel dom
 2. chiedi con un promt 10 numeri
-3.stampa i 10 numeri
+3.stampa la somma
 */
 
 const text = document.getElementById("text");
 // console.log(text);
-
-for (i = 0; i < 10; i++) {
+somma = 0;
+for (i = 1; i <= 10; i++) {
   let i = parseInt(prompt("che numeri vuoi?").trim());
   console.log(i);
+  somma += i;
+  console.log(somma);
 }
+
+text.innerHTML = "la somma dei numeri è = " + somma;

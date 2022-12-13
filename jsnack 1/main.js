b92c599bf825e07f5464a79c4bc072285946afad
LeonardoContino <leoconti2000@hotmail.it>
2022-12-13 15:15:23 +0100

    commenti

--- jsnack 1/main.js
+++ jsnack 1/main.js
@@ -1,1 +1,10 @@
-console.log("js ok");
+// console.log("js ok");
+// SNACK 1
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+//  (qui potete usare un prompt). Il programma stampa la somma di tutti i numeri inseriti.
+
+/*
+1.aggiungi elementi nel dom
+2. chiedi con un promt 10 numeri
+3.stampa i 10 numeri
+*/

6fe77bceb2923edd3a04d18cb1e1cf58fa06b3f1
LeonardoContino <leoconti2000@hotmail.it>
2022-12-13 15:53:46 +0100

    aggiunto input e btn

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -1,1 +1,22 @@
-console.log("js ok");
+// console.log("js ok");
+
+/*
+SNACK 2
+In un array sono contenuti i nomi degli invitati alla festa del grande
+ Gatsby, l’utente inserisce il suo nome tramite un input.
+ comunicagli se può partecipare o no alla festa. 
+(vietato usare includes() )
+ */
+
+/*
+1. scrivere gli elmenti in html
+2.prendere gli elementi dal dom
+3.scrivere una lista con array con dei nomi
+4. fai un for con denrtro gli array
+5. valida se puoi enrtrare o meno
+ */
+
+const input = document.getElementById("input");
+console.log(input);
+const btn = document.getElementById("btn");
+console.log(btn);
